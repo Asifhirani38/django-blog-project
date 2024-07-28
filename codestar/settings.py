9ce71c4d8ee715b436d5b1ea40d1e3e23ec3670e
Asif Hirani <asifhirani@hotmail.com>
2024-07-28 15:05:59 +0000

    added changes for heroku deployment including installing gunicorn

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b(y0gmbk#4(bb6d)_c^h2ccw6x$#vgcalb1b12)l61kq&$t!e8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-asifhirani3-djangoblogp-4tjsbfrkc76.ws.codeinstitute-ide.net']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    '.herokuapp.com',
 ]
 
 MIDDLEWARE = [
